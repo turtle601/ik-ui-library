3ea9f0eb9c4d52ff3af0953800942e606d7fb70e
turtle601 <turtle601@naver.com>
2025-03-18 13:15:17 +0900

    feat: container 컴포넌트 구현

---
+++ src/ui/layout/container.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CSSObject } from '@emotion/react';
+
+import type { PolymorpicProps } from 'src/ui/@types/polymorpic';
+
+interface ContainerProps {
+  maxWidth: CSSObject['maxWidth'];
+  minWdith: CSSObject['maxWidth'];
+  etcStyles?: CSSObject;
+}
+
+function Container<T extends React.ElementType = 'div'>({
+  as,
+  maxWidth,
+  minWdith,
+  etcStyles = {},
+  children,
+  ...attribute
+}: PolymorpicProps<T, ContainerProps>) {
+  const Element = as || 'div';
+
+  return (
+    <Element
+      css={{
+        maxWidth: maxWidth,
+        minWidth: minWdith,
+        ...etcStyles,
+      }}
+      {...attribute}
+    >
+      {children}
+    </Element>
+  );
+}
+
+export default Container;
--- src/ui/layout/index.ts
+++ src/ui/layout/index.ts
@@ -1,2 +1,3 @@
 export { default as Flex } from './flex';
 export { default as Center } from './center';
+export { default as Container } from './container';
