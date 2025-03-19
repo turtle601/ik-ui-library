a5b67a448a80ec8744b590b1b4ca2829092059f8
turtle601 <turtle601@naver.com>
2025-03-19 13:30:34 +0900

    feat: text 컴포넌트 구현

---
+++ src/ui/common/text.tsx
@@ -0,0 +1,47 @@
+import { ElementType } from 'react';
+import { css, CSSObject } from '@emotion/react';
+
+import { color, fontSize, fontWeight } from 'src/config/styles';
+
+import type { PolymorpicProps } from 'src/ui/@types/polymorpic';
+
+export interface ITextProps {
+  /** 텍스트 메세지 */
+  label: string;
+  /** 텍스트 크기 */
+  textSize?: keyof typeof fontSize;
+  /** 텍스트 굵기 (ex. bold, regular ... ) */
+  textWeight?: keyof typeof fontWeight;
+  /** 텍스트 색상 */
+  textColor?: keyof typeof color;
+  /** 그 외 커스텀 설정 */
+  etcStyles?: CSSObject;
+}
+
+function Text<T extends ElementType = 'span'>({
+  as,
+  textSize = 'md',
+  textColor = 'gray400',
+  textWeight = 'regular',
+  label,
+  etcStyles,
+  ...attribute
+}: PolymorpicProps<T, ITextProps>) {
+  const Element = as || 'span';
+
+  return (
+    <Element
+      css={css({
+        color: color[textColor],
+        fontWeight: fontWeight[textWeight],
+        fontSize: fontSize[textSize],
+        ...etcStyles,
+      })}
+      {...attribute}
+    >
+      {label}
+    </Element>
+  );
+}
+
+export default Text;
