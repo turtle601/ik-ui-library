a0152efbb8fd64b99998e035f9435b38d5a530a6
turtle601 <turtle601@naver.com>
2025-03-25 17:20:24 +0900

    fix: pagination export 문제 해결

--- src/ui/common/index.ts
+++ src/ui/common/index.ts
@@ -3,3 +3,4 @@ export { default as Text } from './text';
 export { Tab } from './tab';
 export { Sidebar } from './sidebar';
 export { Modal } from './modal';
+export { Pagination } from './pagination';
--- src/ui/common/pagination/ui/index.ts
+++ src/ui/common/pagination/ui/index.ts
@@ -4,12 +4,10 @@ import PrevBtn from './prevBtn';
 import Provider from './provider';
 import BtnContainer from './btnContainer';
 
-const Pagination = {
+export const Pagination = {
   NextBtn,
   PrevBtn,
   BtnContainer,
   NumBtn,
   Provider,
 } as const;
-
-export default Pagination;
