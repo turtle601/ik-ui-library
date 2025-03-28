7a08b2d957fd0a670a546c64eed480d6a4033e90
turtle601 <turtle601@naver.com>
2025-03-28 13:22:29 +0900

    fix: pagination 현재 페이지 props 설정 추가

--- src/ui/common/pagination/ui/provider.tsx
+++ src/ui/common/pagination/ui/provider.tsx
@@ -2,11 +2,12 @@ import { useCallback, useMemo, useState } from 'react';
 import { PaginationContext } from '../model';
 
 export interface IProviderProps {
+  currentPageNum?: number;
   children: React.ReactNode;
 }
 
-function Provider({ children }: IProviderProps) {
-  const [currentPage, setCurrentPage] = useState(1);
+function Provider({ currentPageNum = 1, children }: IProviderProps) {
+  const [currentPage, setCurrentPage] = useState(currentPageNum);
 
   const goPrevPage = useCallback(() => {
     setCurrentPage((page) => page - 1);
